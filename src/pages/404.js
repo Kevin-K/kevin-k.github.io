e9e4ca447b7db5978d1ecd860143f120df6137ea
Kevin Kelly <kevin@kevinjkelly.net>
2018-10-18 01:01:04 -0400

    Update 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,6 +3,8 @@ import Card from '@material-ui/core/Card';
 import { withStyles } from '@material-ui/core/styles';
 import Layout from '../components/layout';
 import { card } from '../styles';
+import withRoot from '../withRoot';
+import Typography from '@material-ui/core/Typography';
 
 // Build out the styles from the common libraries
 const styles = theme => ({
@@ -16,11 +18,11 @@ const NotFoundPage = props => {
   return (
     <Layout {...other}>
       <Card className={classes.card}>
-        <h4>
+        <Typography variant="body1">
           404: Page not found.
-        </h4>
+        </Typography>
       </Card>
     </Layout>);
 };
 
-export default withStyles(styles)(NotFoundPage);
+export default withRoot(withStyles(styles)(NotFoundPage));
