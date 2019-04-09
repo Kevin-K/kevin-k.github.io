372d8a7d59f268294b99684541eae023e6c4051c
Kevin Kelly <kevin@kevinjkelly.net>
2019-04-08 21:23:02 -0400

    CSS: card fill 100% width

--- src/styles/index.js
+++ src/styles/index.js
@@ -6,4 +6,7 @@ export const card = theme => ({
   textAlign: 'center',
   padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 4}px`,
   margin: theme.spacing.unit * 2,
+  // fill the parent width with the card even if
+  // not filling the card fully
+  width: '100%',
 });
