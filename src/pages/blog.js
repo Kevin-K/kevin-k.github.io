338139e3f6a4d22a3bdff9eea1a173e5a6a03650
Kevin Kelly <kevin@kevinjkelly.net>
2020-06-20 13:52:15 -0400

    Cleanup blog post lists

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,39 +1,15 @@
 import React from 'react';
-import { Link } from 'gatsby';
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-
-const useStyles = makeStyles(theme => ({
-  article: {
-    marginTop: theme.spacing(2),
-    marginBottom: theme.spacing(4),
-  },
-  link: {
-    textDecoration: 'none',
-  },
-}));
-
-const Post = ({ path, title, description, date }) => {
-  const classes = useStyles();
-  return (
-    <div className={classes.article}>
-      <Typography variant="h2">
-        <Link to={path} className={classes.link}>
-          {title}
-        </Link>
-      </Typography>
-      <Typography variant="subtitle1">{date}</Typography>
-      <Typography variant="subtitle2">{description}</Typography>
-    </div>
-  );
-};
+import Post from '../components/blog/post';
 
 export default function BlogPage({ data }) {
   const posts = data.allMdx.edges;
   return (
     <div>
-      {posts.map(({ node }) => (
-        <Post {...node.frontmatter} />
+      {posts.map(({ node }, index) => (
+        <React.Fragment key={index}>
+          <Post {...node.frontmatter} />
+          {index !== (posts.length - 1) && <hr />}
+        </React.Fragment>
       ))}
     </div>
   );
