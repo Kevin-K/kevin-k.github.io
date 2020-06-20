928f469426b5ed1ba1d2c683da51c4a480b68f80
Kevin Kelly <kevin@kevinjkelly.net>
2020-06-20 15:55:25 -0400

    Drop duplicate css baseline

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { makeStyles } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import { useStaticQuery, graphql } from 'gatsby';
 import Nav, { navHeight } from './nav';
 import Paper from '@material-ui/core/Paper';
@@ -42,10 +41,8 @@ export default function Layout({ children, location }) {
   const data = useStaticQuery(query);
   return (
     <>
-      <CssBaseline />
       <Helmet title={data.site.siteMetadata.title} meta={[]}>
         <html lang="en" />
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Helmet>
       <Nav
         title={data.site.siteMetadata.title}
