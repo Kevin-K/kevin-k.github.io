99e96d747103cb03151c7a486fe6657562bcd651
Kevin Kelly <kevin@kevinjkelly.net>
2020-06-20 15:10:49 -0400

    WIP: testing deploy without css baseline

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -42,7 +42,6 @@ export default function Layout({ children, location }) {
   const data = useStaticQuery(query);
   return (
     <>
-      <CssBaseline />
       <Helmet title={data.site.siteMetadata.title} meta={[]}>
         <html lang="en" />
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
